397060cd4f79567364e44368b8857f009043dd29
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2023-02-12 07:17:41 +0530

    admin: Create form for adding new bonds

--- admin/src/pages/NewBond/NewBond.jsx
+++ admin/src/pages/NewBond/NewBond.jsx
@@ -11,80 +11,124 @@ const NewBond = () => {
       <div className="newBondContainer">
         <Navbar />
         <div className="container">
-          <p className="title">New Bond</p>
+          <p className="title">Add Bond</p>
           <div className="row">
             <div className="col">
               <div className="row t1">SYMBOL</div>
-              <div className="row"> <input type="text" name="symbol"/></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="symbol" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">SERIES</div>
-              <div className="row"> <input type="text" name="series" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="series" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">BOND TYPE</div>
-              <div className="row"> <input type="text" name="btype" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="btype" />
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col">
               <div className="row t1">OPEN</div>
-              <div className="row"> <input type="text" name="open" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="open" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">HIGH</div>
-              <div className="row"> <input type="text" name="high" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="high" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">LOW</div>
-              <div className="row"> <input type="text" name="low" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="low" />
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col">
               <div className="row t1">LTP</div>
-              <div className="row"> <input type="text" name="ltp" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="ltp" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">CLOSES</div>
-              <div className="row"> <input type="text" name="closes" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="closes" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">%CHNG</div>
-              <div className="row"> <input type="text" name="chng" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="chng" />
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col">
               <div className="row t1">Quantity</div>
-              <div className="row"> <input type="text" name="qty" /></div>
-
+              <div className="row">
+                {' '}
+                <input type="text" name="qty" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">Value</div>
-              <div className="row"> <input type="text" name="value" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="value" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">COUPON RATE</div>
-              <div className="row"> <input type="text" name="cpnrate" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="cpnrate" />
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col">
               <div className="row t1">CREDIT RATING</div>
-              <div className="row"> <input type="text" name="creditrate" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="creditrate" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">RAING AGENCY</div>
-              <div className="row"> <input type="text" name="ratingagency" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="ratingagency" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">FACE VALUE</div>
-              <div className="row"> <input type="text" name="facevalue" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="facevalue" />
+              </div>
             </div>
           </div>
 
@@ -92,35 +136,53 @@ const NewBond = () => {
             <div className="col">
               {/* Calendar */}
               <div className="row t1">MATURITY DATE</div>
-              <div className="row mt-4"> <input type="date" name="matdate" /></div>
+              <div className="row mt-4">
+                {' '}
+                <input type="date" name="matdate" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">BYEILD</div>
-              <div className="row"> <input type="text" name="byeild" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="byeild" />
+              </div>
             </div>
             <div className="col">
-            <div className="row t1">IS IN</div>
-              <div className="row"> <input type="text" name="isin" /></div>
+              <div className="row t1">IS IN</div>
+              <div className="row">
+                {' '}
+                <input type="text" name="isin" />
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col">
               <div className="row t1">COMPANY NAME</div>
-              <div className="row"> <input type="text" name="cmpname" /></div>
+              <div className="row">
+                {' '}
+                <input type="text" name="cmpname" />
+              </div>
             </div>
             <div className="col">
               <div className="row t1">RELEASED ON</div>
-              <div className="row mt-4"> <input type="date" name="reldate" /></div>
+              <div className="row mt-4">
+                {' '}
+                <input type="date" name="reldate" />
+              </div>
             </div>
             <div className="col subBtn">
-              
-            <button type="button" class="btn btn-primary">Add New Bond</button>
-            <Link to="/bonds"><button type="button" class="btn btn-secondary ms-3">Cancel</button></Link>
-            
+              <button type="button" class="btn btn-primary">
+                Add New Bond
+              </button>
+              <Link to="/bonds">
+                <button type="button" class="btn btn-secondary ms-3">
+                  Cancel
+                </button>
+              </Link>
             </div>
           </div>
-
         </div>
       </div>
     </div>
